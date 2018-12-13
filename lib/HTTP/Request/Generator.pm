8f907c0699dbe35208de9feab1491720588364f9
Max Maischein <corion@corion.net>
2018-12-13 12:47:32 +0100

    Update Changes, documentation

--- lib/HTTP/Request/Generator.pm
+++ lib/HTTP/Request/Generator.pm
@@ -448,4 +448,31 @@ sub as_plack($req) {
 
 L<The Curl Manpage|https://curl.haxx.se/docs/manpage.html> for the pattern syntax
 
+=head1 REPOSITORY
+
+The public repository of this module is
+L<https://github.com/Corion/HTTP-Request-Generator>.
+
+=head1 SUPPORT
+
+The public support forum of this module is L<https://perlmonks.org/>.
+
+=head1 BUG TRACKER
+
+Please report bugs in this module via the RT CPAN bug queue at
+L<https://rt.cpan.org/Public/Dist/Display.html?Name=HTTP-Request-Generator>
+or via mail to L<HTTP-Request-Generator-Bugs@rt.cpan.org|mailto:HTTP-Request-Generator-Bugs@rt.cpan.org>.
+
+=head1 AUTHOR
+
+Max Maischein C<corion@cpan.org>
+
+=head1 COPYRIGHT (c)
+
+Copyright 2017-2018 by Max Maischein C<corion@cpan.org>.
+
+=head1 LICENSE
+
+This module is released under the same terms as Perl itself.
+
 =cut
