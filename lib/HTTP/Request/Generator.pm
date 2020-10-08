374449754a804ea1fa3c0bf9500bba847ed6995b
Max Maischein <corion@corion.net>
2020-10-08 12:19:37 +0200

    Update Changes, META.*, bump version

--- lib/HTTP/Request/Generator.pm
+++ lib/HTTP/Request/Generator.pm
@@ -59,7 +59,7 @@ HTTP::Request::Generator - generate HTTP requests
 
 =cut
 
-our $VERSION = '0.09';
+our $VERSION = '0.10';
 our @EXPORT_OK = qw( generate_requests as_dancer as_plack as_http_request
     expand_curl_pattern
 );
